ea9d014443b5cd152380e29771d12b113d96d84e
nicklhw <nicklhw@gmail.com>
2024-01-26 20:21:47 -0500

    added azure worker and windows VM

--- terraform/azure.tf
+++ terraform/azure.tf
@@ -74,19 +74,29 @@ resource "azurerm_public_ip" "boundary_ingress_worker_pub_ip" {
   domain_name_label   = var.prefix
 }
 
-resource "azurerm_network_interface" "boundary_demo" {
-  name                = "${var.prefix}-nic"
+resource "azurerm_network_interface" "public_subnet_nic" {
+  name                = "${var.prefix}-public-subnet-nic"
   location            = azurerm_resource_group.boundary_demo.location
   resource_group_name = azurerm_resource_group.boundary_demo.name
   ip_configuration {
-    name                          = "${var.prefix}-nic-ipconfig"
+    name                          = "${var.prefix}-public-subnet-nic-ipconfig"
     subnet_id                     = azurerm_subnet.public.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id          = azurerm_public_ip.boundary_ingress_worker_pub_ip.id
   }
 }
 
-# Create an Ubuntu Virtual Machine
+resource "azurerm_network_interface" "private_subnet_nic" {
+  name                = "${var.prefix}-private-subnet-nic"
+  location            = azurerm_resource_group.boundary_demo.location
+  resource_group_name = azurerm_resource_group.boundary_demo.name
+  ip_configuration {
+    name                          = "${var.prefix}-private-subnet-nic-ipconfig"
+    subnet_id                     = azurerm_subnet.private.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
 resource "azurerm_linux_virtual_machine" "boundary_ingress_worker" {
   name                  = "boundary-ingress-worker"
   computer_name         = "boundary-ingress-worker"
@@ -94,8 +104,8 @@ resource "azurerm_linux_virtual_machine" "boundary_ingress_worker" {
   location              = azurerm_resource_group.boundary_demo.location
   size                  = "Standard_A2_v2"
   admin_username        = "adminuser"
-  network_interface_ids = [azurerm_network_interface.boundary_demo.id]
-  custom_data = data.cloudinit_config.azure_boundary_ingress_worker.rendered
+  network_interface_ids = [azurerm_network_interface.public_subnet_nic.id]
+  custom_data           = data.cloudinit_config.azure_boundary_ingress_worker.rendered
 
   admin_ssh_key {
     username   = "adminuser"
@@ -116,7 +126,7 @@ resource "azurerm_linux_virtual_machine" "boundary_ingress_worker" {
 }
 
 # Associate the Network Security Group to the Public Subnet
-resource "azurerm_subnet_network_security_group_association" "boundary_demo" {
+resource "azurerm_subnet_network_security_group_association" "public" {
   subnet_id                 = azurerm_subnet.public.id
   network_security_group_id = azurerm_network_security_group.boundary_ingress_worker_sg.id
 }
@@ -194,4 +204,62 @@ data "cloudinit_config" "azure_boundary_ingress_worker" {
     sudo systemctl start boundary
     EOF
   }
+}
+
+resource "azurerm_windows_virtual_machine" "boundary_demo" {
+  name                  = "boundary-rdp-target"
+  computer_name         = "boundary-target"
+  resource_group_name   = azurerm_resource_group.boundary_demo.name
+  location              = azurerm_resource_group.boundary_demo.location
+  size                  = "Standard_A2_v2"
+  admin_username        = "adminuser"
+  admin_password        = var.windows_admin_password
+  network_interface_ids = [azurerm_network_interface.private_subnet_nic.id]
+
+  source_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2019-Datacenter"
+    version   = "latest"
+  }
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"
+  }
+}
+
+resource "azurerm_network_security_group" "boundary_target_sg" {
+  name                = "${var.prefix}-target-sg"
+  location            = var.azure_location
+  resource_group_name = azurerm_resource_group.boundary_demo.name
+
+  security_rule {
+    name                       = "SSH"
+    priority                   = 101
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefixes    = azurerm_subnet.public.address_prefixes
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "RDP"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "3389"
+    source_address_prefixes    = azurerm_subnet.public.address_prefixes
+    destination_address_prefix = "*"
+  }
+}
+
+resource "azurerm_subnet_network_security_group_association" "private" {
+  subnet_id                 = azurerm_subnet.private.id
+  network_security_group_id = azurerm_network_security_group.boundary_target_sg.id
 }
